40769a4d9dda1afa565a878432e2b7186c47de4a
shizuku86 <shizuku86710@gmail.com>
2026-03-31 10:58:53 +0900

    created DiscoveryQueryParser

---
+++ src/router/errors/DiscoveryQueryParserError.ts
@@ -0,0 +1,20 @@
+export class DiscoveryQueryParseError extends Error {
+    public readonly reason: DiscoveryQueryParseErrorReason;
+
+    constructor(reason: DiscoveryQueryParseErrorReason) {
+        super(DEFAULT_MESSAGES[reason]);
+
+        this.name = "DiscoveryQueryParseError";
+        this.reason = reason;
+    }
+}
+
+export enum DiscoveryQueryParseErrorReason {
+    InvalidJSON = "InvalidJSON",
+    InvalidStructure = "InvalidStructure",
+}
+
+const DEFAULT_MESSAGES: Record<DiscoveryQueryParseErrorReason, string> = {
+    [DiscoveryQueryParseErrorReason.InvalidJSON]: "Failed to parse DiscoveryQuery JSON.",
+    [DiscoveryQueryParseErrorReason.InvalidStructure]: "Invalid DiscoveryQuery structure.",
+};
--- src/router/init/discovery/DiscoveryQueryParser.ts
+++ src/router/init/discovery/DiscoveryQueryParser.ts
@@ -1,3 +1,7 @@
+import {
+    DiscoveryQueryParseError,
+    DiscoveryQueryParseErrorReason,
+} from "../../errors/DiscoveryQueryParserError";
 import { AddonDiscoveryManager } from "./AddonDiscoveryManager";
 import { DiscoveryQuery } from "./DiscoveryQuery";
 
@@ -5,5 +9,37 @@ import { DiscoveryQuery } from "./DiscoveryQuery";
 export class DiscoveryQueryParser {
     public constructor(manager: AddonDiscoveryManager) {}
 
-    public parse(message: string): DiscoveryQuery {}
+    public parse(message: string): DiscoveryQuery {
+        let parsed: unknown;
+
+        try {
+            parsed = JSON.parse(message);
+        } catch {
+            throw new DiscoveryQueryParseError(DiscoveryQueryParseErrorReason.InvalidJSON);
+        }
+
+        this.assertQuery(parsed);
+
+        return parsed;
+    }
+
+    private assertQuery(query: unknown): asserts query is DiscoveryQuery {
+        if (!this.validateQuery(query)) {
+            throw new DiscoveryQueryParseError(DiscoveryQueryParseErrorReason.InvalidStructure);
+        }
+    }
+
+    private validateQuery(query: unknown): boolean {
+        if (typeof query !== "object" || query === null) return false;
+
+        const q = query as any;
+
+        if (typeof q.scoreboard !== "object" || q.scoreboard === null) return false;
+        if (typeof q.scoreboard.objective !== "object" || q.scoreboard.objective === null)
+            return false;
+
+        const objective = q.scoreboard.objective;
+
+        return typeof objective.id === "string" && objective.displayName === "kairo:id_checker";
+    }
 }
