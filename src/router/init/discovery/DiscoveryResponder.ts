a7eb21fa56c0023a4861cabda7443349a58ab5ce
shizuku86 <shizuku86710@gmail.com>
2026-04-08 09:08:27 +0900

    added fast-json-stringify

--- src/router/init/discovery/DiscoveryResponder.ts
+++ src/router/init/discovery/DiscoveryResponder.ts
@@ -1,8 +1,36 @@
+import { system } from "@minecraft/server";
 import { AddonDiscoveryManager } from "./AddonDiscoveryManager";
+import { DiscoveryEventId } from "./constants/DiscoveryEvent";
+import { DiscoveryResponse } from "./types/DiscoveryResponse";
+
+import fastJson from "fast-json-stringify";
 
 // kjs-router-ch 006
 export class DiscoveryResponder {
-    public constructor(manager: AddonDiscoveryManager) {}
+    private readonly discoveryResponseSchema = {
+        type: "object",
+        properties: {
+            addonId: { type: "string" },
+            timestamp: { type: "number" },
+        },
+        required: ["addonId", "timestamp"],
+        additionalProperties: false,
+    } as const;
+    private readonly stringifyDiscoveryResponse: (response: DiscoveryResponse) => string;
 
-    public respond(addonId: string): void {}
+    public constructor(manager: AddonDiscoveryManager) {
+        this.stringifyDiscoveryResponse = fastJson(this.discoveryResponseSchema);
+    }
+
+    public respond(addonId: string): void {
+        const response: DiscoveryResponse = {
+            addonId,
+            timestamp: system.currentTick,
+        };
+
+        system.sendScriptEvent(
+            DiscoveryEventId.Response,
+            this.stringifyDiscoveryResponse(response),
+        );
+    }
 }
---
+++ src/router/init/discovery/types/DiscoveryResponse.ts
@@ -0,0 +1,4 @@
+export interface DiscoveryResponse {
+    readonly addonId: string;
+    readonly timestamp: number;
+}
