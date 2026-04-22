3fd8840ff1dde2b1b439de6ecbe4b0d6bbd21b58
shizuku86 <shizuku86710@gmail.com>
2026-04-22 16:27:11 +0900

    Simplify playerJoin event binding

--- src/minecraft/minecraftEventBinding.ts
+++ src/minecraft/minecraftEventBinding.ts
@@ -2,20 +2,9 @@ import { EventBindingSpec } from "../router/types/EventBindingSpec";
 
 export const minecraftEventBinding: EventBindingSpec = {
     after: {
-        playerJoin: (world, handler: (p: any) => void) => {
-            const listener = (ev: any) => {
-                handler({
-                    playerId: ev.playerId,
-                    playerName: ev.playerName,
-                });
-            };
-
-            world.afterEvents.playerJoin.subscribe(listener);
-
-            return {
-                dispose: () => world.afterEvents.playerJoin.unsubscribe(listener),
-            };
-        },
+        playerJoin: (world, handler) => {
+            return world.afterEvents.playerJoin.subscribe(handler);
+        }
     },
 
     before: {},
